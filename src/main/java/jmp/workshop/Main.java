f9bb05b41aebdbc34dc72079d953017786564356
Bakhodir Marupov <bakhodirjon_marupov@epam.com>
2022-07-22 16:39:19 +0500

    initial commit

---
+++ src/main/java/jmp/workshop/Main.java
@@ -0,0 +1,13 @@
+package jmp.workshop;
+
+/**
+ * Author: ${USER}
+ * Date: ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        String type = System.getenv().get("type");
+
+        System.out.println("Hello world!");
+    }
+}
