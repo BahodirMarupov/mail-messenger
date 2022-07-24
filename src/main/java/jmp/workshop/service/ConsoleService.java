abde62ef8d5e28716f47c21ff985b44c6febc838
Bakhodir Marupov <bakhodirjon_marupov@epam.com>
2022-07-25 01:56:11 +0500

    ConsoleService methods written

--- src/main/java/jmp/workshop/service/ConsoleService.java
+++ src/main/java/jmp/workshop/service/ConsoleService.java
@@ -1,6 +1,8 @@
 package jmp.workshop.service;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Author: Bakhodirjon_Marupov
@@ -9,11 +11,23 @@ import java.util.Map;
 public class ConsoleService {
 
     public String readTemplate() {
-        return null;
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 
     public Map<String, String> readPlaceholders() {
-        return null;
+        Map<String, String> placeholders = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (input != null && input.length() > 0) {
+            for (String s : input.split(";")) {
+                String[] elements = s.split(">");
+                if (elements.length == 2) {
+                    placeholders.put(elements[0], elements[1]);
+                }
+            }
+        }
+        return placeholders;
     }
 
 }
