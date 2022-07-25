c1bca22db9d6884f6816e2ff564f2711bfaf9f66
Bakhodir Marupov <bakhodirjon_marupov@epam.com>
2022-07-25 20:15:42 +0500

    some changes

--- src/main/java/jmp/workshop/Main.java
+++ src/main/java/jmp/workshop/Main.java
@@ -1,13 +1,22 @@
 package jmp.workshop;
 
+import jmp.workshop.service.ConsoleService;
+import jmp.workshop.service.FileService;
+import jmp.workshop.service.MessengerService;
+import jmp.workshop.service.TemplateService;
+
 /**
- * Author: ${USER}
- * Date: ${DATE}
+ * Author: Bakhodirjon_Marupov
+ * Date: 24/07/2022
  */
 public class Main {
-    public static void main(String[] args) {
-        String type = System.getenv().get("type");
 
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        ConsoleService consoleService = new ConsoleService();
+        FileService fileService = new FileService();
+        TemplateService templateService = new TemplateService();
+
+        MessengerService messengerService = new MessengerService(consoleService, fileService, templateService);
+        messengerService.start(args);
     }
 }
