d6c79de21a3aaf25069239d7b8130fde65d53966
Bakhodir Marupov <bakhodirjon_marupov@epam.com>
2022-07-25 19:26:38 +0500

    MessengerService methods written

--- src/main/java/jmp/workshop/service/MessengerService.java
+++ src/main/java/jmp/workshop/service/MessengerService.java
@@ -1,5 +1,7 @@
 package jmp.workshop.service;
 
+import java.util.Map;
+
 /**
  * Author: Bakhodirjon_Marupov
  * Date: 24/07/2022
@@ -17,7 +19,30 @@ public class MessengerService {
     }
 
     public void start(String[] paths) {
-
+        if (paths != null) {
+            switch (paths.length) {
+                case 0:
+                    tryWithConsole();
+                    return;
+                case 2:
+                    tryWithFile(paths[0], paths[1]);
+                    return;
+            }
+        }
+        throw new IllegalArgumentException("Invalid parameters");
     }
 
+    private void tryWithConsole() {
+        String template = consoleService.readTemplate();
+        Map<String, String> placeholders = consoleService.readPlaceholders();
+        String message = templateService.prepareMessage(template, placeholders);
+        System.out.println(message);
+    }
+
+    private void tryWithFile(String inputFilePath, String outputFilePath) {
+        String template = fileService.readTemplate(inputFilePath);
+        Map<String, String> placeholders = consoleService.readPlaceholders();
+        String message = templateService.prepareMessage(template, placeholders);
+        fileService.writeMessage(outputFilePath, message);
+    }
 }
