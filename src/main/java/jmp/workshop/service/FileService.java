848423db025968583d20b5732ff64a646a00f6f1
Bakhodir Marupov <bakhodirjon_marupov@epam.com>
2022-07-25 15:15:16 +0500

    FileService methods written

--- src/main/java/jmp/workshop/service/FileService.java
+++ src/main/java/jmp/workshop/service/FileService.java
@@ -1,5 +1,9 @@
 package jmp.workshop.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /**
  * Author: Bakhodirjon_Marupov
  * Date: 24/07/2022
@@ -7,10 +11,18 @@ package jmp.workshop.service;
 public class FileService {
 
     public String readTemplate(String inputFilePath) {
-        return null;
+        try {
+            return Files.readString(Path.of(inputFilePath));
+        } catch (IOException e) {
+            throw new RuntimeException("Exception when reading from file");
+        }
     }
 
     public void writeMessage(String outputFilePath, String message) {
-
+        try {
+            Files.writeString(Path.of(outputFilePath), message);
+        } catch (IOException e) {
+            throw new RuntimeException("Exception when writing to file");
+        }
     }
 }
